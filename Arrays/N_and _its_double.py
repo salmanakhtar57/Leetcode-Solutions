4e336557a353d574c1c4eb8b7e26e7ee03017350
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-10 22:02:31 +0500

    learning binary search

---
+++ Arrays/N_and _its_double.py
@@ -0,0 +1,15 @@
+"""
+Given an array arr of integers, check if there exist two indices i and j such that :
+
+i != j
+0 <= i, j < arr.length
+arr[i] == 2 * arr[j]
+
+example 1:
+Input: arr = [10,2,5,3]
+Output: true
+
+example 2:
+Input: arr = [3, 1, 7, 11]
+Output: false
+"""
