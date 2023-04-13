707c73d0a6055decb5b492ac51f00d8f85e04548
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-04-13 22:19:50 +0500

    checking n and its double solved

--- Arrays/N_and _its_double.py
+++ Arrays/N_and _its_double.py
@@ -13,3 +13,15 @@ example 2:
 Input: arr = [3, 1, 7, 11]
 Output: false
 """
+
+class Solution:
+    def checkIfExist(self, arr) -> bool:
+        double = set()
+        for i in range(len(arr)):
+            if 2*arr[i] in double:
+                return True
+            double.add(arr[i])
+        return False
+
+print(Solution().checkIfExist([10,2,5,3]))
+print(Solution().checkIfExist([3, 1, 7, 11]))
