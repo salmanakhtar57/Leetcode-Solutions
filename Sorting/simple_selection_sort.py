6ad60b80687c4ff754c846a019b54ca0d9b39d93
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-03 20:33:14 +0500

    sort colors

--- Sorting/simple_selection_sort.py
+++ Sorting/simple_selection_sort.py
@@ -8,10 +8,10 @@ def selection_sort(nums):
         nums[i], nums[min_index] = nums[min_index], nums[i]
     return nums
 
-print(selection_sort([2,0,2,1,1,0]))
+print(selection_sort([2,0,2,1,1,0])) #output: [0, 0, 1, 1, 2, 2]
 
 #Python Built in sort() method
 nums = [2,0,2,1,1,0]
 nums.sort()
 result = nums
-print(result)
+print(result) #output: [0, 0, 1, 1, 2, 2]
--- Sorting/sort_colors.py
+++ Sorting/sort_colors.py
@@ -13,8 +13,21 @@ Output: [0,1,2]
 """
 
 class Solution:
-    def sortColors(self, nums, int):
-        min_index = 0
-        max_index = len(nums) - 1
+    def sortColors(self, nums):
+        red, white, blue = 0, 0, len(nums)-1
+        while white <= blue:
 
-        for num in nums:
+            if nums[white] == 0:
+                nums[red], nums[white] = nums[white], nums[red]
+                white += 1
+                red += 1
+            elif nums[white] == 1:
+                white += 1
+            else:
+                nums[white], nums[blue] = nums[blue], nums[white]
+                blue -= 1
+        return nums
+
+s = Solution()
+s.sortColors([2,0,2,1,1,0])
+print(s.sortColors([2,0,2,1,1,0])) #output: [0, 0, 1, 1, 2, 2]
