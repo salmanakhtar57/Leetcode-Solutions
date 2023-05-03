249285cb666eb285102de2dc31ddc27ba6e7c61f
Salman Akhtar <98455899+salmanakhtar57@users.noreply.github.com>
2023-05-03 20:16:31 +0500

    implement insertion sort in Python

---
+++ Sorting/simple_selection_sort.py
@@ -0,0 +1,17 @@
+#insertion sort
+def selection_sort(nums):
+    for i in range(len(nums)):
+        min_index = i
+        for j in range(i+1, len(nums)):
+            if nums[min_index] > nums[j]:
+                min_index = j
+        nums[i], nums[min_index] = nums[min_index], nums[i]
+    return nums
+
+print(selection_sort([2,0,2,1,1,0]))
+
+#Python Built in sort() method
+nums = [2,0,2,1,1,0]
+nums.sort()
+result = nums
+print(result)
